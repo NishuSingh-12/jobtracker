f12f5f4c07645b362d944f868288ca36035e8166
Nishu singh <nishu.frontend@gmail.com>
2026-02-11 20:23:25 +0530

    vite react + tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-amber-200">
+      <h1 className="text-4xl font-bold text-black">Tailwind v4 Working ✅</h1>
+    </div>
+  );
+}
